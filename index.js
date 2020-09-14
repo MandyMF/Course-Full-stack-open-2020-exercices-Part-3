e2a9e2ab6cb9d16cc8a66ee0a98b5798b276fadd
MandyMF <mandy@fonoma.com>
2020-09-14 00:25:55 -0400

    Exercise 3.7

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
+//middleware
 app.use(express.json())
+
+//all work, pick your poison
+//app.use(morgan('tiny'))
+//app.use(morgan(':method :url :status :res[content-length] :response-time ms'))
+
+//using a function
+
+app.use(morgan (function(tokens, req, res){
+    return [
+        tokens.method(req,res),
+        tokens.url(req,res),
+        tokens.status(req,res),
+        tokens.res(req,res, 'content-length'),
+        '-',
+        tokens['response-time'](req, res), 'ms'
+    ].join(' ')
+}))
+
+//creating a token and using it:
+/*morgan.token('my-tiny', function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'),
+        '-',
+        tokens['response-time'](req, res), 'ms'
+    ].join(' ')}
+)
+app.use(morgan('my-tiny'))*/
+//------------------------------
+
+
 
 let persons = [
     { id: 1, name: 'Arto Hellas', number: '040-123456' },
@@ -52,27 +87,25 @@ const generateNewId = () => {
     return Math.floor(Math.random() * 999999999999)
 }
 
-app.post('/api/persons/', (req, res)=>{
+app.post('/api/persons/', (req, res) => {
     const body = req.body
-    
-    if(!body.name || !body.number)
-    {
+
+    if (!body.name || !body.number) {
         return res.status(400).json({
-            error:'body and name are needed'
+            error: 'body and name are needed'
         })
     }
-    if (persons.find(person=> person.name === body.name))
-    {
+    if (persons.find(person => person.name === body.name)) {
         return res.status(409).json({
-            error:'name must be unique'
+            error: 'name must be unique'
         })
     }
-    
-    const new_person=
+
+    const new_person =
     {
         name: body.name,
         number: body.number,
-        id:generateNewId()
+        id: generateNewId()
     }
 
     persons = persons.concat(new_person)
