d3f2700e3c53aca7dbf567b0fd4f99848f9d6228
MandyMF <mandy@fonoma.com>
2020-09-13 20:23:46 -0400

    Exercise 3.3

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    {id:1, name: 'Arto Hellas', number: '040-123456' },
-    {id:2, name: 'Ada Lovelace', number: '39-44-5323523' },
-    {id:3, name: 'Dan Abramov', number: '12-43-234345' },
-    {id:4, name: 'Mary Poppendieck', number: '39-23-6423122' }
+    { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
 
 ]
 
-app.get('/api/persons', (req, res)=>{
+app.get('/api/persons', (req, res) => {
 
     res.json(persons)
 })
 
-app.get('/info', (req, res)=>{
+app.get('/info', (req, res) => {
 
     res.send(
         `<p>
@@ -28,6 +28,20 @@ app.get('/info', (req, res)=>{
     )
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
 
-const PORT=3001
-app.listen(PORT, ()=> console.log(`Server is runnning on port ${PORT}`))
+    const person_requested = persons.find((person)=> person.id === id)
+
+    if(person_requested){
+        res.json(person_requested)
+    }
+    else {
+        res.status(404).end()
+    }
+
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`Server is runnning on port ${PORT}`))
