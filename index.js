58c34607f999f852b98d031d5fc63f3bdc5458c3
MandyMF <mandy@fonoma.com>
2020-09-19 18:47:43 -0400

    Exercise 3.17*

--- index.js
+++ index.js
@@ -102,6 +102,20 @@ app.delete('/api/persons/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id',(req, res, next)=>{
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, {new:true})
+    .then(updatedPerson =>{
+        res.json(updatedPerson)
+    }).catch(error => next(error))
+})
+
 app.post('/api/persons/', (req, res) => {
     const body = req.body
 
